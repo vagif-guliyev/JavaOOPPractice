c5f64c243b25d5d65f35f20fbfe02465272fd96b
vagif-guliyev <vaguliyev@gmail.com>
2023-01-07 21:56:54 +0400

    How static block works

---
+++ User.java
@@ -0,0 +1,8 @@
+public class User {
+    static {
+        System.out.println("Static block is called");
+    }
+    public static void foo() {
+        System.out.println("Foo is called");
+    }
+}
