debe91d93b6c4e4f93c8bfbf0cbd0869651fa13f
vagif-guliyev <vaguliyev@gmail.com>
2023-01-07 22:06:11 +0400

    How Non-static/Instance Block works

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("User");
+
+        new User();
+        new User(5);
     }
 }
--- User.java
+++ User.java
@@ -2,4 +2,16 @@ public class User {
     static {
         System.out.println("Static block is called");
     }
+
+    {
+        System.out.println("Non-static/Instance block is called");
+    }
+
+    public User() {
+        System.out.println("Empty Constructor");
+    }
+
+    public User(int a) {
+        System.out.println("Numbered Constructor");
+    }
 }
